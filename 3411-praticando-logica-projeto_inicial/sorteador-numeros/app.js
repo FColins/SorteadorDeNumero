f2911e8b7c31f735957e3f397831e4dbd6612e5f
Felipe Colins <felipe_colins@hotmail.com>
2025-12-12 12:37:53 +0000

    Alert Numeros

--- 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -10,6 +10,11 @@ function sortear(){
         numero = obterNumeroAleatorio(de, ate);
         sorteados.push(numero);
     }
-    
-    alert(sorteados);
+
+    let resultado = document.getElementById('resultado')
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`
+} 
+
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
